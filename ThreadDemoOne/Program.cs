d0fc7fd1f0bced23595296c626651600714b77b3
Karabo <mtimkulukarabo@gmail.com>
2018-10-02 10:49:06 +0200

    Initial commit

---
+++ ThreadDemoOne/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadDemoOne
+{
+    class Program
+    {
+        public static void ThreadMethod(object o)
+        {
+            for (int i = 0; i < (int)o; i++)
+            {
+                Console.WriteLine("ThreadProc: { 0}", i);
+            Thread.Sleep(0);
+        }
+    }
+    static void Main()
+        {
+
+        bool stopped = false;
+
+        Thread t = new Thread(new ThreadStart(() =>
+        {
+            while (!stopped)
+            {
+                Console.WriteLine("Running...");
+                Thread.Sleep(1000);
+            }
+        }));
+
+
+        t.Start();
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        stopped = true;
+        t.Join();
+
+
+    }
+
+    }
+
+}
